b8f9bf8ef819519bd616405c7b9fcc73129b29c5
mehrdad moradalaei <mehrdad.moradali@gmail.com>
2023-12-05 21:03:35 +0100

    chk

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import './components/Billboard';
+import Billboard from './components/Billboard';
+
+function App() {
+  return (
+  <Billboard/>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Billboard.js
@@ -0,0 +1,10 @@
+import "./Billboard.css";
+
+function Billboard(){
+    return(
+        <div>
+            it is just start, do not give up!
+        </div>
+    );
+}
+export default Billboard;
