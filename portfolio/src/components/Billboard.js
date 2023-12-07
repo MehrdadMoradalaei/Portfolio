819c939efe70d56ebe83a4f96776f0a4306c1097
mehrdad moradalaei <mehrdad.moradali@gmail.com>
2023-12-07 11:22:19 +0100

    responsive billboard

--- portfolio/src/components/Billboard.js
+++ portfolio/src/components/Billboard.js
@@ -11,6 +11,12 @@ function Billboard(){
                  </div>
             </div>
             <div className="rightSide">
+               <div className="description">
+                  <h4>Junior Software Developer</h4>
+                  <h1>Mehrdad Moradalaei</h1>
+                  <p>I am a person who follows his dreams and his interests. Sometimes your second favorite career is so attractive that it makes you change your whole world. In my case, "Programing" has changed my world</p>
+
+               </div>
             </div>
 
        </div>
