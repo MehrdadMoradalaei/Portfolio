2233089d693e753ece3db6358382bb54571cba40
mehrdad moradalaei <mehrdad.moradali@gmail.com>
2024-01-13 17:45:24 +0100

    buttons

--- portfolio/src/components/Billboard.js
+++ portfolio/src/components/Billboard.js
@@ -1,25 +1,34 @@
+import React from 'react';
+import cvPic from "../assets/cvPic.jpg";
+import pdfFile from "../assets/CV-Mehrdad Moradalaei.pdf";
 import "./Billboard.css";
-import cvPic from "../assets/cvPic.jpg"
 
-function Billboard(){
-    return(
-       <div className="container">
-            <div className="leftSide">
-                 <div className="title">Mehrdad Moradalaei</div>
-                 <div>
-                    <img src={cvPic} className="img" alt=""></img>
-                 </div>
-            </div>
-            <div className="rightSide">
-               <div className="description">
-                  <h4>Junior Software Developer</h4>
-                  <h1>Mehrdad Moradalaei</h1>
-                  <p>I am a person who follows his dreams and his interests. Sometimes your second favorite career is so attractive that it makes you change your whole world. In my case, "Programing" has changed my world</p>
-
-               </div>
-            </div>
-
-       </div>
-    );
+function Billboard() {
+  return (
+    <div className="container">
+      <div className="leftSide">
+        <div className="title">Mehrdad Moradalaei</div>
+        <div>
+          <img src={cvPic} className="img" alt="" />
+        </div>
+      </div>
+      <div className="rightSide">
+        <div className="description">
+          <h2>Junior Software Developer</h2>
+          <h1>Mehrdad Moradalaei</h1>
+          <p>
+            I am a person who follows his dreams and his interests. Sometimes your second favorite career is so attractive that it makes you change your whole world. In my case, "Programing" has changed my world
+          </p>
+          <div className="buttonContainer">
+            <a className='link' href={pdfFile} download="CV-Mehrdad Moradalaei" target="_blank" rel="noopener noreferrer">
+              <div className="cvButton">Download CV</div>
+            </a>
+            <div className="contactButton">Contact</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Billboard;
